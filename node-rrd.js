98dac9afd3fc028842d8c20459954e87539ccbd2
Steve <steve@carbon.trease.eu>
2017-01-14 15:07:51 +0000

    Rebaseline

--- node-rrd.js
+++ node-rrd.js
@@ -15,30 +15,30 @@ var mapping = require('./mappings.json');
 
 var mqtt = require('mqtt');
 var mqttclient = mqtt.connect(config.mqtt.host, {
-	username: "bridge",
-	password: "qdJ0jrkHgOUWMioMu0iOteDnhnjpqu2Riv6E2qoJAgtEUrqRwtWjVkGPuUvfbwt"
+    username: config.mqtt.username,
+    password: config.mqtt.password
 });
 
 
 
 mqttclient.on('connect', function() {
-	mqttclient.subscribe('sensors/+/+');
-	mqttclient.subscribe('sensors/+/+/+');
+    mqttclient.subscribe('sensors/+/+');
+    mqttclient.subscribe('sensors/+/+/+');
 
 
-	mqttclient.on('message', function(topic, message) {
-		// console.error(topic, message.toString());
-		
-		if (mapping[topic] === undefined) {
-			console.error (topic + ": no mapping array entry");
-		} else {		
-			// what will the rrd file be called? (removing /s from the string)
-			var filename = "data/" + mapping[topic].file;
-			
-			var value = message.toString();
-			// var now = Math.ceil((new Date).getTime() / 1000);
-			
-			console.log ("rrdtool update " + filename + " N:" + value);
-		}
-	});
+    mqttclient.on('message', function(topic, message) {
+        // console.error(topic, message.toString());
+
+        if (mapping[topic] === undefined) {
+            console.error (topic + ": no mapping array entry");
+        } else {
+            // what will the rrd file be called? (removing /s from the string)
+            var filename = "data/" + mapping[topic].file;
+
+            var value = message.toString();
+            // var now = Math.ceil((new Date).getTime() / 1000);
+
+            console.log ("rrdtool update " + filename + " N:" + value);
+        }
+    });
 });
